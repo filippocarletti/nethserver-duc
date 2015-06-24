694c35762427c25d050f09608d6875431e55e2bf
Davide Principi <davide.principi@nethesis.it>
2015-06-24 14:31:12 +0200

    Removed Italian translations.

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_Duc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* NethServer_Module_Dashboard_Duc translation, language: it */
-$L['Duc_Title'] = 'Utilizzo disco';
-$L['Update_label'] = 'Aggiorna';
-$L['Updated on ${0}'] = 'Aggiornato il: ${0}';
-$L['Not_Updated_Duc_Label'] = 'Filesystem non indicizzato. Clicca Aggiorna.';
-$L['Date_Format'] = 'd-m-Y H:i:s';
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Duc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$L['Duc_Title'] = 'Analisi disco e utilizzo';
-$L['Duc_Description'] = 'Analisi spazio e utilizzo del disco';
-$L['Duc_Tags'] = 'disco analisi spazio utilizzo duc';
-
-$L['Duc_label'] = 'Abilita l\'analisi del disco';
-$L['DucStatus_enabled_label'] = 'Abilita';
-$L['DucStatus_disabled_label'] = 'Disabilita';
